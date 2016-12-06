863bf301b2580a930ec74332d3d4cc4237b29d58
thieril <thieril@dunwoody.edu>
2016-12-06 17:09:10 -0600

    Add project files.

---
+++ GatoHaveItFinal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GatoHaveItFinal.MvcApplication" Language="C#" %>
---
+++ GatoHaveItFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GatoHaveItFinal.Startup))]
+namespace GatoHaveItFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
